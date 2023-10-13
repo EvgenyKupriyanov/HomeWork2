3986f38080961741e2c9559fb9fcffe844809244
Evgeny <geny_91@mail.ru>
2023-10-13 11:31:29 +0300

    Task29

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+     for(int i = 0; i < array.Length; i++)
+     {
+        if (array[i] > 0)
+        {count++;
+        }  
+     }
+     Console.Write(count);
+     
+}
+
+Console.Write("Введите числа: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ReleaseArray(array);
